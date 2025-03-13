b3600869aeb8c58e1fc305060eae8da079f1b9f2
Swarnim Shukla <swarnim0300@gmail.com>
2025-03-14 03:10:31 +0530

    improvement on services start

--- service-registery/src/main/java/com/eureka/serviceregistery/ServiceRegisteryApplication.java
+++ service-registery/src/main/java/com/eureka/serviceregistery/ServiceRegisteryApplication.java
@@ -3,15 +3,13 @@ package com.eureka.serviceregistery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableHystrix
-public class ServiceRegisteryApplication {
+public class EurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceRegisteryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
 
 }
